38294fd834742cf7c84ca7d837a3500c7e680033
enginnerturph <enginnerturph@github.com>
2022-06-13 23:28:00 +0300

    gunun son commiti

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(50.0, 10.0, 30.0, 20.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.red,
+            child: Text('Hello'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+//Buranin icindeki degisirse sadece bu kisim degisiyor
+//yeniden acmak yerine hot restart daha ii
